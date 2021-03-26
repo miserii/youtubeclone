0e6806ed28bbdf9777150619b92312cc3c91832d
miseri <miseri.osk12@gmail.com>
2021-03-27 02:30:03 +0900

    add collectionView

--- youtube/ViewController.swift
+++ youtube/ViewController.swift
@@ -9,9 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let cellId = "Cell"
+
+    @IBOutlet private weak var videoListCollectionView: UICollectionView! {
+        didSet {
+            videoListCollectionView.delegate = self
+            videoListCollectionView.dataSource = self
+            videoListCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        videoListCollectionView.backgroundColor = .red
+    }
+
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.width
+
+        return .init(width: width, height: width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .green
+
+        return cell
     }
 
 
